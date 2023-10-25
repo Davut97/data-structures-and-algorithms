0649bcb5d6f69effc79cc054e74cbb77858d4b5e
Aydin Davutoglu <aydindavit14@gmail.com>
2023-10-25 14:56:46 +0300

    integer reversal done

---
+++ integer-reversal/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(reverseInteger(123))
+}
+
+func reverseInteger(original int) int {
+	num := original
+	reversed := 0
+
+	for num != 0 {
+		digit := num % 10
+		reversed = reversed*10 + digit
+		num = num / 10
+	}
+
+	return reversed
+}
