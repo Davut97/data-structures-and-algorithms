070c4cf40d0719cf9c1db90486240f8cbbeda72a
Aydin Davutoglu <aydindavut14@gmail.com>
2023-10-29 21:26:34 +0300

    anagrams

---
+++ anagrams/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println(anagrams("rail safety", "fairy tales"))
+}
+
+// func anagrams(stringA, stringB string) bool {
+// 	m1 := make(map[byte]int)
+// 	m2 := make(map[byte]int)
+// 	for i := 0; i < len(stringA); i++ {
+// 		m1[stringA[i]] += 1
+// 	}
+// 	for i := 0; i < len(stringB); i++ {
+// 		m2[stringB[i]] += 1
+// 	}
+
+//		return reflect.DeepEqual(m1, m2)
+//	}
+func anagrams(stringA, stringB string) bool {
+
+	return sortString(stringA) == sortString(stringB)
+
+}
+func sortString(input string) string {
+	strChars := strings.Split(input, "")
+	sort.Strings(strChars)
+
+	return strings.Join(strChars, "")
+}
